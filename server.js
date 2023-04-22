6681abe4594bd6627f83a943b364ea9dae0ddd87
AHachey17 <ahachey17@gmail.com>
2023-04-21 22:34:50 -0400

    re-push

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var db, collection;
 const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
 const dbName = "demo";
 
-app.listen(3000, () => {
+app.listen(4000, () => {
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
         if(error) {
             throw error;
@@ -22,27 +22,48 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
-
+//everything above boiler plate
 app.get('/', (req, res) => {
+  //go into db college get messgaes in an array
   db.collection('messages').find().toArray((err, result) => {
     if (err) return console.log(err)
+    //pass messages into index.ejs:
     res.render('index.ejs', {messages: result})
   })
 })
 
+//w.e stored in req input we get the doc that has body na
 app.post('/messages', (req, res) => {
   db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
+    //sent something to database, to retrieve get request then refresh:
     res.redirect('/')
   })
 })
 
-app.put('/messages', (req, res) => {
+
+
+app.put('/messages/thumbUp', (req, res) => {
+  db.collection('messages')
+  .findOneAn dUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbUp + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.put('/messages/thumbDown', (req, res) => {
   db.collection('messages')
   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
     $set: {
-      thumbUp:req.body.thumbUp + 1
+      thumbUp: req.body.thumbDown - 1
     }
   }, {
     sort: {_id: -1},
